d4c131d7b926787a4d3285e77a0f86d6b429b773
Chris <christiandc19@Chriss-MBP.lan>
2022-05-04 22:23:33 -0700

    created components

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs'
+import { BsDribbble } from 'react-icons/bs'
+
+const HeaderSocials = () => {
+    return (
+    <div className="header__socials">
+        <a href="http://linkedIn.com" target='_blank'><BsLinkedin/></a>
+        <a href="http://github.com" target='_blank'><BsGithub/></a>
+        <a href="http://dribble.com" target='_blank'><BsDribbble/></a>
+    </div>
+    )
+}
+
+export default HeaderSocials;
+
